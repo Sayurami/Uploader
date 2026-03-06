eacf3014dfc28e275fbe52d995eddfbd0f6be411
Sayurami <sayuramihiranga4@gmail.com>
2026-03-06 11:25:14 +0530

    Update sessions.js

--- api/sessions.js
+++ api/sessions.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = "mongodb+srv://Sayuracinema:Sayura2008@cluster0.a0263gy.mongodb.net/Sayura_DB?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URI = "mongodb+srv://sayuaradark_db_user:password@cluster0.w8wb15r.mongodb.net/MyBotDB?retryWrites=true&w=majority&appName=Cluster0";
 // ================= MongoDB Schema =================
 const credsSchema = new mongoose.Schema({
   sessionId: { type: String, required: true, unique: true },
